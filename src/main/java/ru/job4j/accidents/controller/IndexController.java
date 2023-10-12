1767b62468eb2129670512d7b0be71ad7dbc6a9d
apereslavtsev <87319362+apereslavtsev@users.noreply.github.com>
2023-10-12 19:13:12 +0000

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class IndexController {
 
     @GetMapping({"/", "/index", "/accidents"})
     public String getIndex(Model model) {
-        model.addAttribute("user", "Anton Pereslavtsev");
+       model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.getAll());
         return "index";
     }
