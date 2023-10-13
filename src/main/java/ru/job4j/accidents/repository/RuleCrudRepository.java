c937293e8676ee3b0438edeb004e7da52f7e0fb7
apereslavtsev <87319362+apereslavtsev@users.noreply.github.com>
2023-10-13 17:50:53 +0000

    2. Spring Data | [#296073]

--- src/main/java/ru/job4j/accidents/repository/RuleCrudRepository.java
+++ src/main/java/ru/job4j/accidents/repository/RuleCrudRepository.java
@@ -2,15 +2,11 @@ package ru.job4j.accidents.repository;
 
 import java.util.List;
 
-import org.assertj.core.util.Lists;
 import org.springframework.data.repository.CrudRepository;
-
 import ru.job4j.accidents.model.Rule;
 
 public interface RuleCrudRepository extends CrudRepository<Rule, Integer> {
 
-    public default List<Rule> findAll() {
-        return Lists.newArrayList(findAll());
-    }
+    List<Rule> findAll();
 
 }
