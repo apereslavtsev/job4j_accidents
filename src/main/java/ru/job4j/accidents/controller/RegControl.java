35c8bc02d60adb713bddaeedcf0c8fba3363a6c6
apereslavtsev <87319362+apereslavtsev@users.noreply.github.com>
2023-10-19 18:35:57 +0000

    2. Регистрация пользователя [#296069 #453437]

--- src/main/java/ru/job4j/accidents/controller/RegControl.java
+++ src/main/java/ru/job4j/accidents/controller/RegControl.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,12 @@ public class RegControl {
     private final AuthorityRepository authorities;
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
+        if (users.findByUsername(user.getUsername()).isPresent()) {
+            model.addAttribute("errorMessage",
+            "User with this login already exists, select a different login");
+            return "reg";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
--- src/main/java/ru/job4j/accidents/repository/UserRepository.java
+++ src/main/java/ru/job4j/accidents/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package ru.job4j.accidents.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByUsername(String name);
+
 }
